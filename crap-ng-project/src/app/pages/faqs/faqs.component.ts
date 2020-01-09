c353cf553eb3224e9904ee602559a430b65d897f
Robert Burns <robertburns316@gmail.com>
2020-01-09 15:25:28 -0800

    some changes

--- crap-ng-project/src/app/pages/faqs/faqs.component.ts
+++ crap-ng-project/src/app/pages/faqs/faqs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { on } from 'cluster';
 
 @Component({
   selector: 'app-faqs',
@@ -6,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./faqs.component.scss']
 })
 export class FaqsComponent implements OnInit {
+  flip = false;
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  clickEvent(e) {
+    console.log(e);
+    // alert('You clicked me, prepare to die! IN')
+  }
+
+  hoverExit(e) {
+    console.log(e);
+    // alert('You clicked me, prepare to die! OUT')
+  }
+
+  switch(v) {
+    console.log(v);
+    this.flip = this.flip ? !this.flip : !!!this.flip;
+    
+    v.innerText = this.flip ? 'Off' : 'On';
+    
+    // this.flip = !this.flip;
+    
+    // if(this.flip){
+    //   v.innerText = 'Off';
+    // } else{
+    //   v.innerText = 'On';
+    // }
+
+    // if (this.flip) {
+    //   this.flip = false;
+    // } else {
+    //   this.flip = true;
+    // }
+  }
+
 
 }
--- crap-ng-project/src/app/pages/home/home.component.ts
+++ crap-ng-project/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Student } from 'src/app/student';
 
 @Component({
   selector: 'app-home',
@@ -6,11 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  trash = [
-    {title:'My Project 1', desc: 'Trash', nFO: 'something else'},
-    {title:'My Project 2', desc: 'Trash 2', nFO: 'something else 2'},
-    {title:'My Project 3', desc: 'Trash 3', nFO: 'something else 3'},
-    
+  // trash = [
+  //   {title:'My Project 1', desc: 'Trash', nFO: 'something else', img: 'assets/images/random-angular-background.jpg', hLink: 'https://getbootstrap.com/docs/4.4/components/card/'},
+  //   {title:'My Project 2', desc: 'Trash 2', nFO: 'something else 2', img: 'assets/images/random-angular-image.jpg', hLink: 'https://git-scm.com/book/en/v2/Getting-Started-First-Time-Git-Setup'},
+  //   {title:'My Project 3', desc: 'Trash 3', nFO: 'something else 3', img: 'assets/images/random-angular-image2.jpg', hLink: 'https://github.com/Burns316/NG-Projects'},
+
+  // ];
+  trash: Student[] = [
+    {
+      fName: 'Jon',
+      lName: 'Stewart',
+      sName: 'JonStewart',
+      pNumber: '44444444',
+    },
+    {
+      fName: 'Joe',
+      lName: 'Dontknow',
+      sName: 'mofo',
+      pNumber: '8888888',
+    },
+    {
+      fName: 'Rob',
+      lName: 'Burns',
+      sName: 'Robert Burns',
+      pNumber: '3573454684',
+    }
   ];
   constructor() { }
 
