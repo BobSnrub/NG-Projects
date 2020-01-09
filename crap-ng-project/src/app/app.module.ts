4fb9cccc2dade526ffb67ccbb06533a0455baae2
Robert Burns <robertburns316@gmail.com>
2020-01-09 11:23:05 -0800

    installed bootstrap packages and made some components

--- crap-ng-project/src/app/app.module.ts
+++ crap-ng-project/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { FaqsComponent } from './pages/faqs/faqs.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainComponent,
+    FaqsComponent,
+    HomeComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
