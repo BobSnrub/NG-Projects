03f3ac23cf020a335809fd20124fa68663cee648
Robert Burns <robertburns316@gmail.com>
2020-02-04 22:26:11 -0800

    10:26 PM 2/4/2020

---
+++ burns-portfolio/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { MainComponent } from './shared/components/main/main.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
